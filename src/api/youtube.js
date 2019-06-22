c86b8ca69fca983776340cc639f316c769c198df
Brad Bailey <bbailey921@gmail.com>
2019-06-22 16:19:08 -0400

    add onVideoSelect to state and added type: video to youtube api

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import VideoList from '../components/VideoList';
 
 const KEY = 'AIzaSyCgDq3Ywwq3pPNcS1yHWuR60sp6NqW-ti8';
 
@@ -6,6 +7,7 @@ export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
     params: {
         part: 'snippet',
+        type: 'video',
         maxResults: 5,
         key: KEY,
     }
--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import SearchBar from './SearchBar';
 import VideoList from './VideoList';
 
 class App extends React.Component {
-    state = { videos: [] };
+    state = { videos: [], selectedVideo: null };
 
     onTermSubmit = async term => {
         const response = await youtube
@@ -17,11 +17,15 @@ class App extends React.Component {
         this.setState({ videos: response.data.items });
     };
 
+    onVideoSelect = video => {
+        console.log('From the app!', video);
+    };
+
     render() {
         return (
         <div className="ui container">
             <SearchBar onFormSubmit={this.onTermSubmit} />
-            <VideoList videos={this.state.videos} />
+            <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
         </div>
         );
     }
