47a87d62c508b13c86fd5bcd5ca099190331cb52
Brad Bailey <bbailey921@gmail.com>
2019-06-22 14:47:47 -0400

    add event listener to change state of search component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,6 +3,10 @@ import React from 'react';
 class SearchBar extends React.Component {
     state = { term: '' };
 
+    onInputChange = e => {
+        this.setState({ term: e.target.value });
+    };
+
     render() {
         return (
             <div className="search-bar ui segment">
@@ -12,6 +16,7 @@ class SearchBar extends React.Component {
                         <input 
                         type="text"
                         value={this.state.term}
+                        onChange={this.onInputChange}
                         />
                     </div>
                 </form>
